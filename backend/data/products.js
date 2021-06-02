8f3a27e20393e14002f39c330d2f624d0a957b5e
tiennlv <lvt11072000@gmail.com>
2021-06-02 23:23:37 +0700

    git add data to import

---
+++ backend/data/products.js
@@ -0,0 +1,56 @@
+const products = [
+  {
+    name: "PlayStation 5",
+    price: 499,
+    imageUrl:
+      "https://images.unsplash.com/photo-1606813907291-d86efa9b94db?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1352&q=80",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Eget dolor morbi non arcu risus quis varius quam. A diam sollicitudin tempor id eu nisl nunc mi. Ullamcorper a lacus vestibulum sed arcu. Turpis cursus in hac habitasse platea dictumst quisque sagittis. At elementum eu facilisis sed.",
+    quantity: 15,
+    brand: "PlayStation",
+    color: "white",
+    category: "ps",
+    rate: 4,
+  },
+  {
+    name: "Iphone 12",
+    price: 1099,
+    imageUrl:
+      "https://images.unsplash.com/photo-1605787020600-b9ebd5df1d07?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1463&q=80",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Eget dolor morbi non arcu risus quis varius quam. A diam sollicitudin tempor id eu nisl nunc mi. Ullamcorper a lacus vestibulum sed arcu. Turpis cursus in hac habitasse platea dictumst quisque sagittis. At elementum eu facilisis sed.",
+    quantity: 10,
+    brand: "Apple",
+    color: "white",
+    category: "phone",
+    rate: 5,
+  },
+  {
+    name: "Amazon Alexa",
+    imageUrl:
+      "https://images.unsplash.com/photo-1518444065439-e933c06ce9cd?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1267&q=80",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Eget dolor morbi non arcu risus quis varius quam. A diam sollicitudin tempor id eu nisl nunc mi. Ullamcorper a lacus vestibulum sed arcu. Turpis cursus in hac habitasse platea dictumst quisque sagittis. At elementum eu facilisis sed.",
+    price: 50,
+    quantity: 25,
+    brand: "Apple",
+    color: "white",
+    category: "virtual assistant",
+    rate: 3.5,
+  },
+  {
+    name: "JBL FLIP 4",
+    price: 140,
+    imageUrl:
+      "https://images.unsplash.com/photo-1564424224827-cd24b8915874?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1489&q=80",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Eget dolor morbi non arcu risus quis varius quam. A diam sollicitudin tempor id eu nisl nunc mi. Ullamcorper a lacus vestibulum sed arcu. Turpis cursus in hac habitasse platea dictumst quisque sagittis. At elementum eu facilisis sed.",
+    quantity: 10,
+    brand: "JBL",
+    color: "black",
+    category: "speaker",
+    rate: 4,
+  },
+];
+
+module.exports = products;
