e3bfb50935852fb12f7a67ecf4cf19adb9383111
tiennlv <lvt11072000@gmail.com>
2021-06-02 23:24:52 +0700

    add more property for product model

---
+++ backend/models/Product.js
@@ -0,0 +1,32 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const productSchema = new Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  price: {
+    type: Number,
+    required: true,
+  },
+  imageUrl: {
+    type: String,
+    required: true,
+  },
+  quantity: {
+    type: Number,
+    required: true,
+  },
+  rate: {
+    type: Number,
+    required: true,
+  },
+  color: String,
+  brand: String,
+  category: String,
+  description: String,
+});
+
+const Product = mongoose.model("Product", productSchema);
+module.exports = Product;
