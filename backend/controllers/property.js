53f09e5ae899e8ac748ebd4030a634cd60defc30
tiennlv <lvt11072000@gmail.com>
2021-06-03 23:46:35 +0700

    add controller to get property of product

---
+++ backend/controllers/property.js
@@ -0,0 +1,55 @@
+const Product = require("../models/Product");
+
+const getCategories = async (req, res) => {
+  try {
+    const categories = await Product.aggregate([
+      {
+        $group: {
+          _id: "$category",
+        },
+      },
+    ]);
+    res.json(categories.map((c) => c._id));
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ message: "Server Error" });
+  }
+};
+
+const getBrands = async (req, res) => {
+  try {
+    const brands = await Product.aggregate([
+      {
+        $group: {
+          _id: "$brand",
+        },
+      },
+    ]);
+    res.json(brands.map((c) => c._id));
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ message: "Server Error" });
+  }
+};
+
+const getColors = async (req, res) => {
+  try {
+    const colors = await Product.aggregate([
+      {
+        $group: {
+          _id: "$color",
+        },
+      },
+    ]);
+    res.json(colors.map((c) => c._id));
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ message: "Server Error" });
+  }
+};
+
+module.exports = {
+  getCategories,
+  getColors,
+  getBrands,
+};
