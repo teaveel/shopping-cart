1faf599bcd855879d4bca5d211201bff174e6675
tiennlv <lvt11072000@gmail.com>
2021-06-05 16:10:54 +0700

    delelte color property

--- backend/models/Product.js
+++ backend/models/Product.js
@@ -1,32 +1,34 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
-const productSchema = new Schema({
-  name: {
-    type: String,
-    required: true,
+const productSchema = new Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    price: {
+      type: Number,
+      required: true,
+    },
+    image: {
+      type: String,
+      required: true,
+    },
+    quantity: {
+      type: Number,
+      required: true,
+    },
+    rate: {
+      type: Number,
+      required: true,
+    },
+    brand: String,
+    category: String,
+    description: String,
   },
-  price: {
-    type: Number,
-    required: true,
-  },
-  image: {
-    type: String,
-    required: true,
-  },
-  quantity: {
-    type: Number,
-    required: true,
-  },
-  rate: {
-    type: Number,
-    required: true,
-  },
-  color: String,
-  brand: String,
-  category: String,
-  description: String,
-});
+  { timestamps: true }
+);
 
 const Product = mongoose.model("Product", productSchema);
 module.exports = Product;
